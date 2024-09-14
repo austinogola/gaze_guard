343049259db1db2e2c3199fa2a7596ff3a595477
Anthony Ndirangu <anthonymuriithindirangu@gmail.com>
2024-09-14 18:52:46 +0300

    more styling

--- site/src/components/Features.js
+++ site/src/components/Features.js
@@ -1,47 +1,43 @@
-import {Box,
-    Text,
-    Flex,
-    Image,
-    HStack,
-    Link,
-    IconButton,
-    Button,
-    useDisclosure,
-    useColorModeValue,
-    Stack,} from '@chakra-ui/react'
+import React from "react";
+import "./styles/features.css";
 
-    const SingleFeature=({mainText,expl})=>{
-        return (
-            <Box w='45%' p='10px' px='20px' mb='50px' minWidth='250px'>
-                <Text m='0px' mb='10px' fontSize='25px' fontWeight={500}>{mainText}</Text>
-                <Box w='50px' h='2px' backgroundColor='black'></Box>
-                <Text m='0px' mt='20px' fontSize='20px' fontWeight={400}>
-                    {expl}
-                </Text>
-            </Box>
-        )
-    }
+const SingleFeature = ({ mainText, expl }) => {
+  return (
+    <div className="single-feature">
+      <div className="feature-main-text feature-text">
+        <h2>{mainText}</h2>
+        <div className="feature-divider"></div>
+      </div>
+      <div className="feature-expl feature-text">
+        <p>{expl}</p>
+      </div>
+    </div>
+  );
+};
 
-    const Features=()=>{
-        return(
-        <Flex justifyContent='center' id='features' mb='100px' >
-            <Flex w='80%' flexWrap='wrap' justifyContent='center'>
-            <SingleFeature mainText='Filter Explicit Media'
-                    expl=" Automatically detect and filters out any media content deemed explicit or inappropriate according to your personal or religious standards. Whether you're browsing social media, news websites, or any other online platform, the extension works in real-time to analyze images and videos, blurring or blocking content that doesn't align with your values"                    />
+const Features = () => {
+  return (
+    <div className="features-container" id="features">
+      <div className="features-wrapper">
+        <SingleFeature
+          mainText="Filter Explicit Media"
+          expl="Automatically detect and filter out any media content deemed explicit or inappropriate according to your personal or religious standards. Whether you're browsing social media, news websites, or any other online platform, the extension works in real-time to analyze images and videos, blurring or blocking content that doesn't align with your values."
+        />
+        <SingleFeature
+          mainText="Gender-Based Detection"
+          expl="Experience advanced gender-based detection, allowing you to filter content based on the gender of the individuals featured in the media. Whether you want to avoid images or videos that prominently feature men, women, or both, the extension provides precise control over what you see."
+        />
+        <SingleFeature
+          mainText="Customizable Settings"
+          expl="We understand that everyone has unique needs and preferences, the extension comes with a wide range of customizable settings. You can fine-tune the level of filtering, choose specific types of content to block, and even set different rules for different websites."
+        />
+        <SingleFeature
+          mainText="Seamless Integration"
+          expl="Designed to work effortlessly with your favorite web browsers and online platforms, the extension integrates seamlessly into your daily digital routine. Once installed, it operates quietly in the background, requiring minimal interaction from you."
+        />
+      </div>
+    </div>
+  );
+};
 
-            <SingleFeature mainText='Gender-Based Detection'
-            expl="Experience advanced gender-based detection, allowing you to filter content based on the gender of the individuals featured in the media. Whether you want to avoid images or videos that prominently feature men, women, or both, the extension provides precise control over what you see"                    />
-
-            <SingleFeature mainText='Customizable Settings'
-                    expl='We understand that everyone has unique needs and preferences, the extension comes with a wide range of customizable settings. You can fine-tune the level of filtering, choose specific types of content to block, and even set different rules for different websites'                    />
-
-            <SingleFeature mainText='Seamless Integration'
-            expl='Designed to work effortlessly with your favorite web browsers and online platforms, the extension integrates seamlessly into your daily digital routine. Once installed, it operates quietly in the background, requiring minimal interaction from you'                    />
-            
-            </Flex>
-
-        </Flex>)
-    }
-
-
-    export default Features
+export default Features;
