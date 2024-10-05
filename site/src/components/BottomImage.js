89858e83a0392ae9b00291568ed985dd766056d5
Anthony Ndirangu <anthonymuriithindirangu@gmail.com>
2024-10-05 15:36:45 +0300

    modification

--- site/src/components/BottomImage.js
+++ site/src/components/BottomImage.js
@@ -1,35 +1,29 @@
+import "./styles/BottomImage.css";
+import lapiImg from "../images/lapiBtm.png";
 
-
-import './styles/BottomImage.css'
-import lapiImg from '../images/lapiBtm.png'
-
-import logoImg from '../images/bottomimg.png'
-const BottomImage=()=>{
-    return (
-        <div className='bottomImageDiv'>
-
-
-            <div className='bottom-holder'>
-                <div className='mainImgHolder'>
-                    {/* <img src={btmImg}/> */}
-                </div>
-                <div className='logoImgHolder'>
-                    <img src={logoImg}/>
-                </div>
-                <div className='textDiv'>
-                    <p>Download your</p>
-                    <p><span>Extension </span> from here</p>
-                    <button>DOWNLOAD</button>
-                </div>
-
-                <div className='lapHolder'>
-                    <img src={lapiImg}/>
-                </div>
-            </div>
+import logoImg from "../images/bottomimg.png";
+const BottomImage = () => {
+  return (
+    <div className="parentDiv-bottom">
+      <div className="bottom-holder">
+        <div className="logoImgHolder">
+          <img src={logoImg} alt="bottom-image" />
         </div>
-    )
-}
+        <div className="textDiv">
+          <div>
+            <p>Download your</p>
+            <p>
+              <span>Extension </span> from here
+            </p>
+          </div>
+          <button>DOWNLOAD</button>
+        </div>
+        <div className="lapHolder">
+          <img src={lapiImg} alt="lapi-image" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-
-export default BottomImage
+export default BottomImage;
